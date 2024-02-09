5b342659bdb868ccbfa1f45d94ccef589dc59591
SADDAM WASSAN <100377630+Saddamwassan@users.noreply.github.com>
2024-02-09 18:44:47 +0500

    loop & functions in php

--- index.php
+++ index.php
@@ -279,10 +279,78 @@ $products = [
     ["name"=>"Shirt","price"=>2200],
     ["name"=>"Jacket","price"=>3000]
 ];
-    foreach($products as $pro){
-        echo "{$pro["name"]} - $ {$pro["price"]}<br>";
+    // foreach($products as $pro){
+    //     echo "{$pro["name"]} - $ {$pro["price"]}<br>";
 
+    // }
+    // // while loop***************
+    // $i = 0;
+    // while($i<count($products)){
+    //     echo $products[$i]["name"];
+    //     echo"<br>";
+    //     $i++;
+    // }
+    // comparison operators 
+    // echo "ali">"Ali";
+    // echo "ali"=="ali";
+    // echo "2"== 2;
+    $candidate = [
+        ["name"=>"ShahMehmodQureshi","votes"=>180000,],
+        ["name"=>"ImranKhan","votes"=>240000],
+        ["name"=>"NawazSharif","votes"=>50000],
+        ["name"=>"Bilawal","votes"=>70000],
+        ["name"=>"Zardar","votes"=>100000],
+        ["name"=>"shahbazSharif","votes"=>2],
+    ];
+    // function *****************
+    function election($candidates){
+        echo "{$candidates["name"]} has won {$candidates["votes"]}";
     }
-
-
+    election(["name"=>"IK","votes"=>185000]);
 ?>
+ <!-- php within html ************** -->
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>php</title>
+    </head>
+    <body>
+        <h2>Html with Php</h2>
+        <ul>
+        <?php 
+        foreach($products as $product){ ?>
+        <h3><?php echo $product["name"];?></h3>
+        <p><?php echo $product["price"];?></p>
+
+        <?php } ?>
+        </ul>
+        
+    </body>
+    </html>
+    <!-- conditional statement with html  -->
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <ul>
+            <?php foreach($candidate as $candi){?>
+                <?php if($candi['votes']>100000){?>
+                    <h1>Winner</h1>
+                    <li><?php echo $candi["name"];?></li>
+                    <li><?php echo $candi["votes"];?></li>
+                <?php }?>
+                <?php  if($candi['votes']<100000){?>
+                    <h1>Looser</h1>
+                    <li><?php echo $candi["name"];?></li>
+                    <li><?php echo $candi["votes"];?></li>
+                <?php }?>
+                <?php }?>
+        </ul>
+    </body>
+    </html>
