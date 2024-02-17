c39d40cb08bdef427513dc5207993dda0b8247d6
SADDAM WASSAN <100377630+Saddamwassan@users.noreply.github.com>
2024-02-17 21:31:23 +0500

    php practice

--- form_validation.php
+++ form_validation.php
@@ -1,3 +1,18 @@
+<?php 
+$name = $email = $site = $gender = $site = "";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+$name = $_POST["name"];
+$email = $_POST["email"];
+$gender = $_POST["gender"];
+$site = $_POST["site"];
+}
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,13 +21,21 @@
     <title>form_validation</title>
 </head>
 <body>
-    <form action="form.php" method="post">
+    <form method="post" action ="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
       Name:  <input type="text" name="name">
       email: <input type="text" name= "email">
       portfolio-sit: <input type="text" name="site">
-      Gender: <input type="radio" name="gender" value>
-
-      <input type="submit">
+      Male: <input type="radio" name="gender" value="male">
+      Female: <input type="radio" name="gender" value="female">
+      comment: <textarea name="comment" id="comment" cols="30" rows="10"></textarea> 
+      <input type="submit" name="submit" value="Submit">
     </form>
+    <?php 
+    echo $name."<br>";
+    echo $email."<br>";
+    echo $site."<br>";
+    echo $gender."<br>";
+
+    ?>  
 </body>
 </html>
