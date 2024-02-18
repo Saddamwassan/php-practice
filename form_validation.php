2a22fb4871020d146674ab9d3e7fdd5bcd8bd294
SADDAM WASSAN <100377630+Saddamwassan@users.noreply.github.com>
2024-02-18 15:21:09 +0500

    form validation done

--- form_validation.php
+++ form_validation.php
@@ -1,10 +1,39 @@
 <?php 
-$name = $email = $site = $gender = $site = "";
+$name = $email  = $gender = $site = $comment =  "";
+$nameError = $emailError =  $genderError = "";
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-$name = $_POST["name"];
-$email = $_POST["email"];
-$gender = $_POST["gender"];
-$site = $_POST["site"];
+  // name 
+  if(empty($_POST["name"])){
+    $nameError = "name is required";
+  }else{
+    $name = test_input($_POST["name"]);
+    // if(preg_match('/a-zA-Z[]/',$name))
+  }
+  // email 
+  if(empty($_POST["email"])){
+    $emailError = "email is required";
+  }else{
+    $email = test_input($_POST["email"]);
+  }
+  // gender 
+  if(empty($_POST["gender"])){
+    $genderError = "gender is required";
+  }else{
+    $gender = test_input($_POST["gender"]);
+  }
+  // website 
+  if(empty($_POST["site"])){
+    $site = "";
+  }else{
+    $site = test_input($_POST["site"]);
+  }
+   // comment 
+   if(empty($_POST["comment"])){
+    $comment = "";
+  }else{
+    $comment = test_input($_POST["comment"]);
+  }
+
 }
 function test_input($data) {
   $data = trim($data);
@@ -19,15 +48,34 @@ function test_input($data) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>form_validation</title>
+    <style>
+      span{
+        color:red;
+      }
+      input{
+        margin:10px;
+      }
+    </style>
 </head>
 <body>
     <form method="post" action ="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+      <span>* required field</span>
+      <br>
       Name:  <input type="text" name="name">
+      <span>*<?php echo $nameError?></span>
+      <br>
       email: <input type="text" name= "email">
+      <span>*<?php echo $emailError?></span>
+      <br>
       portfolio-sit: <input type="text" name="site">
+      <br>
       Male: <input type="radio" name="gender" value="male">
+      <span>*<?php echo $genderError?></span>
+      <br>
       Female: <input type="radio" name="gender" value="female">
-      comment: <textarea name="comment" id="comment" cols="30" rows="10"></textarea> 
+      <br>
+      comment: <textarea name="comment" id="comment" cols="30" rows="6"></textarea> 
+      <br>
       <input type="submit" name="submit" value="Submit">
     </form>
     <?php 
@@ -35,7 +83,6 @@ function test_input($data) {
     echo $email."<br>";
     echo $site."<br>";
     echo $gender."<br>";
-
     ?>  
 </body>
 </html>
